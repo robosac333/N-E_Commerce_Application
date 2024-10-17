580d78ffc44e0092eed35c0ff088f5db8d3d23fc
clockno143 <46952530+clockno143@users.noreply.github.com>
2024-10-17 12:54:28 -0400

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -1,6 +1,6 @@
 <?php 
 // $con=mysqli_connect('localhost','root','','ecommerce_1');
-$con = new mysqli('localhost','root','','ecommerce_1');
+//$con = new mysqli('localhost','root','','ecommerce_1');
 if(!$con){
     die(mysqli_error($con));
 }
