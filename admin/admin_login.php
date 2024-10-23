193f001126b1a075855212b0a1d4f8d95dce4b21
AnandA-Ksh1tiJ <anandakshitij1997@gmail.com>
2024-10-23 17:56:05 -0400

    Add CDN location for css

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -10,8 +10,8 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ecommerce Admin Login</title>
-    <link rel="stylesheet" href="../assets/css/bootstrap.css" />
-    <link rel="stylesheet" href="../assets/css/main.css" />
+    <link rel="stylesheet" href="https://d3vbl7vqzfz11g.cloudfront.net/assets/css/bootstrap.css" />
+    <link rel="stylesheet" href="https://d3vbl7vqzfz11g.cloudfront.net/assets/css/main.css" />
 </head>
 
 <body>
--- users_area/confirm_payment.php
+++ users_area/confirm_payment.php
@@ -34,8 +34,8 @@ if(isset($_POST['confirm_payment'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Payment Page</title>
-    <link rel="stylesheet" href="../assets/css/bootstrap.css" />
-    <link rel="stylesheet" href="../assets/css/main.css" />
+    <link rel="stylesheet" href="https://d3vbl7vqzfz11g.cloudfront.net/assets/css/bootstrap.css" />
+    <link rel="stylesheet" href="https://d3vbl7vqzfz11g.cloudfront.net/assets/css/main.css" />
 </head>
 
 <body>
